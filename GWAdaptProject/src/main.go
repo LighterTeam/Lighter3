89d9bb48c15f0919f3e6f785baa37b3463be6f2f
Enel <spiritring@gmail.com>
2013-08-07 14:59:03 +0800

    v1
    v1

---
+++ GWAdaptProject/src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-8-7
+ * Time: 下午2:58
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
